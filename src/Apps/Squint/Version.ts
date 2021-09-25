059380523abbfccc184f491506aef6da54c7196d
Scott Woyak <scott.woyak@gmail.com>
2021-09-25 19:57:18 -0400

    npm update

--- src/Apps/Squint/Version.ts
+++ src/Apps/Squint/Version.ts
@@ -1,7 +1,7 @@
 export class Version {
    public static Major = 0;
    public static Minor = 0;
-   public static Build = '107d';
+   public static Build = '108';
 
    public static toString(): string {
       return this.Major + '.' + this.Minor + '.' + this.Build;
